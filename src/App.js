6f21c0b43aca9b1a858d9d87d1cde5bde3122220
Ernst Salzmann <Ernst.Salzmann@datameer.com>
2017-09-11 21:09:24 +0200

    feat: Added Section Enumerated type
    WHAT: SectionEnum is used to sort books into sections:
      ‘Currently Reading’, ‘Want to Read’, ‘Read’.
    
    HOW: Each book now contains a ‘section’ (i.e. book.section)
    
    DOCS:
    [Enums in
    Javascript](https://stijndewitt.com/2014/01/26/enums-in-javascript)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import { bindAll } from 'lodash';
 // import * as BooksAPI from './BooksAPI'
 import './App.css';
 
+const SectionEnum = {
+    CURRENTLY_READING: 1,
+    WANT_TO_READ: 2,
+    READ: 3,
+    props: {
+      1: {key: 'CURRENTLY_READING', name: 'Currently Reading', value: 1},
+      2: {key: 'WANT_TO_READ', name: 'Want to Read', value: 2},
+      3: {key: 'READ', name: "Read", value: 3}
+    }
+  };
+
 class BooksApp extends React.Component {
 
   state = {
@@ -19,37 +30,44 @@ class BooksApp extends React.Component {
       key: 1,
       url: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
       title: 'To Kill a Mockingbird',
-      author: 'Harper Lee'
+      author: 'Harper Lee',
+      section: SectionEnum.CURRENTLY_READING
     }, {
       key: 2,
       url: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")',
       title: 'Ender`s Game',
-      author: 'Orson Scott Card'
+      author: 'Orson Scott Card',
+      section: SectionEnum.CURRENTLY_READING
     }, {
       key: 3,
       url: 'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")',
       title: '1776',
-      author: 'David McCullough'
+      author: 'David McCullough',
+      section: SectionEnum.WANT_TO_READ
     }, {
       key: 4,
       url: 'url("http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api)',
       title: 'Harry Potter and the Sorcerer`s Stone',
-      author: 'J.K. Rowling'
+      author: 'J.K. Rowling',
+      section: SectionEnum.WANT_TO_READ
     }, {
       key: 5,
       url: 'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api)',
       title: 'The Hobbit',
-      author: 'J.R.R. Tolkien'
+      author: 'J.R.R. Tolkien',
+      section: SectionEnum.READ
     }, {
       key: 6,
       url: 'url("http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api)',
       title: 'Oh, the Places You`ll Go!',
-      author: 'Seuss'
+      author: 'Seuss',
+      section: SectionEnum.READ
     }, {
       key: 7,
       url: 'url("http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api)',
       title: 'The Adventures of Tom Sawyer',
-      author: 'Mark Twain'
+      author: 'Mark Twain',
+      section: SectionEnum.READ
     }
   ];
 
@@ -104,7 +122,10 @@ class BooksApp extends React.Component {
   }
 
   BookList() {
-    const { addBook, Bookshelf, books } = this;
+    const { addBook, Bookshelf, books } = this,
+      currentlyReading = books.filter((book) => book.section === SectionEnum.CURRENTLY_READING),
+      wantToRead = books.filter((book) => book.section === SectionEnum.WANT_TO_READ),
+      read = books.filter((book) => book.section === SectionEnum.READ);
 
     return (
       <div className="list-books">
@@ -113,9 +134,9 @@ class BooksApp extends React.Component {
         </div>
         <div className="list-books-content">
           <div>
-            { Bookshelf('Currently Reading', books.slice(0,2)) }
-            { Bookshelf('Want to Read', books.slice(2,4)) }
-            { Bookshelf('Read', books.slice(4,8)) }
+            { Bookshelf('Currently Reading', currentlyReading) }
+            { Bookshelf('Want to Read', wantToRead) }
+            { Bookshelf('Read', read) }
           </div>
         </div>
         <div className="open-search">
