8fcde53c463d34cd9f464747e42edfff45f05d8d
Ernst Salzmann <Ernst.Salzmann@datameer.com>
2017-09-29 01:04:53 +0200

    refactor: book 'shelf' property -> shelfId

--- src/App.js
+++ src/App.js
@@ -23,46 +23,46 @@ class BooksApp extends React.Component {
 
     books: [{
         id: 1,
-        url: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
+        shelfId: ShelfEnum.CURRENTLY_READING,
         title: 'To Kill a Mockingbird',
         author: 'Harper Lee',
-        shelf: ShelfEnum.CURRENTLY_READING
+        url: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'
       }, {
         id: 2,
-        url: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")',
+        shelfId: ShelfEnum.CURRENTLY_READING,
         title: 'Ender`s Game',
         author: 'Orson Scott Card',
-        shelf: ShelfEnum.CURRENTLY_READING
+        url: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")'
       }, {
         id: 3,
-        url: 'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")',
+        shelfId: ShelfEnum.WANT_TO_READ,
         title: '1776',
         author: 'David McCullough',
-        shelf: ShelfEnum.WANT_TO_READ
+        url: 'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")'
       }, {
         id: 4,
-        url: 'url("http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api)',
         title: 'Harry Potter and the Sorcerer`s Stone',
         author: 'J.K. Rowling',
-        shelf: ShelfEnum.WANT_TO_READ
+        shelfId: ShelfEnum.WANT_TO_READ,
+        url: 'url("http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api)'
       }, {
         id: 5,
-        url: 'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api)',
+        shelfId: ShelfEnum.READ,
         title: 'The Hobbit',
         author: 'J.R.R. Tolkien',
-        shelf: ShelfEnum.READ
+        url: 'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api)',
       }, {
         id: 6,
-        url: 'url("http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api)',
+        shelfId: ShelfEnum.READ,
         title: 'Oh, the Places You`ll Go!',
         author: 'Seuss',
-        shelf: ShelfEnum.READ
+        url: 'url("http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api)'
       }, {
         id: 7,
-        url: 'url("http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api)',
+        shelfId: ShelfEnum.READ,
         title: 'The Adventures of Tom Sawyer',
         author: 'Mark Twain',
-        shelf: ShelfEnum.READ
+        url: 'url("http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api)'
       }]
   }
 
@@ -124,10 +124,9 @@ class BooksApp extends React.Component {
       bookshelves = ShelfEnum.asList
         .filter(shelfEnum => shelfEnum.id !== ShelfEnum.NONE)
         .map(shelfEnum => Bookshelf({
-          id: shelfEnum.id,
+          shelfId: shelfEnum.id,
           title: shelfEnum.name,
-          shelf: shelfEnum.id,
-          books: books.filter((book) => book.shelf === shelfEnum.id).sort(sortBookByTitle)
+          books: books.filter((book) => book.shelfId === shelfEnum.id).sort(sortBookByTitle)
         }));
 
     return (
@@ -147,14 +146,14 @@ class BooksApp extends React.Component {
     )
   }
 
-  Bookshelf({id, title, shelf, books}) {
+  Bookshelf({shelfId, title, books}) {
     const { Book } = this,
       booklist = books.map((book) => {
         return <li key={book.id}>{Book(book)}</li>
       });
 
     return (
-      <div key={id} className="bookshelf">
+      <div key={shelfId} className="bookshelf">
         <h2 className="bookshelf-title">{title}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
@@ -209,11 +208,11 @@ class BooksApp extends React.Component {
     return items;
   }
 
-  onSelectionChange(id, value) {
+  onSelectionChange(id, shelfId) {
     const { books } = this.state,
       index = books.findIndex(x => x.id === id);
 
-    books[index].shelf = Number(value);
+    books[index].shelfId = Number(shelfId);
     this.setState({books});
   }
 
