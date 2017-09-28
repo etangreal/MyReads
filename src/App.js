ba91c877f5f95bb7faecf8e700a4f3f2e5f99fc7
Ernst Salzmann <Ernst.Salzmann@datameer.com>
2017-09-28 23:52:08 +0200

    refactor: renamed key -> id

--- src/App.js
+++ src/App.js
@@ -22,43 +22,43 @@ class BooksApp extends React.Component {
     showSearchPage: false,
 
     books: [{
-        key: 1,
+        id: 1,
         url: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
         title: 'To Kill a Mockingbird',
         author: 'Harper Lee',
         section: SectionEnum.CURRENTLY_READING
       }, {
-        key: 2,
+        id: 2,
         url: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")',
         title: 'Ender`s Game',
         author: 'Orson Scott Card',
         section: SectionEnum.CURRENTLY_READING
       }, {
-        key: 3,
+        id: 3,
         url: 'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")',
         title: '1776',
         author: 'David McCullough',
         section: SectionEnum.WANT_TO_READ
       }, {
-        key: 4,
+        id: 4,
         url: 'url("http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api)',
         title: 'Harry Potter and the Sorcerer`s Stone',
         author: 'J.K. Rowling',
         section: SectionEnum.WANT_TO_READ
       }, {
-        key: 5,
+        id: 5,
         url: 'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api)',
         title: 'The Hobbit',
         author: 'J.R.R. Tolkien',
         section: SectionEnum.READ
       }, {
-        key: 6,
+        id: 6,
         url: 'url("http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api)',
         title: 'Oh, the Places You`ll Go!',
         author: 'Seuss',
         section: SectionEnum.READ
       }, {
-        key: 7,
+        id: 7,
         url: 'url("http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api)',
         title: 'The Adventures of Tom Sawyer',
         author: 'Mark Twain',
@@ -122,12 +122,12 @@ class BooksApp extends React.Component {
       { books } = this.state,
 
       bookshelves = SectionEnum.asList
-        .filter(section => section.key !== SectionEnum.NONE)
+        .filter(section => section.id !== SectionEnum.NONE)
         .map(section => Bookshelf({
-          key: section.key,
+          id: section.id,
           title: section.name,
-          section: section.key,
-          books: books.filter((book) => book.section === section.key).sort(sortBookByTitle)
+          section: section.id,
+          books: books.filter((book) => book.section === section.id).sort(sortBookByTitle)
         }));
 
     return (
@@ -147,14 +147,14 @@ class BooksApp extends React.Component {
     )
   }
 
-  Bookshelf({key, title, section, books}) {
+  Bookshelf({id, title, section, books}) {
     const { Book } = this,
       booklist = books.map((book) => {
-        return <li key={book.key}>{Book(book)}</li>
+        return <li key={book.id}>{Book(book)}</li>
       });
 
     return (
-      <div key={key} className="bookshelf">
+      <div key={id} className="bookshelf">
         <h2 className="bookshelf-title">{title}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
@@ -165,7 +165,7 @@ class BooksApp extends React.Component {
     );
   }
 
-  Book({key, url, title, author, section}) {
+  Book({id, url, title, author, section}) {
     const { Selection } = this;
 
     return (
@@ -179,7 +179,7 @@ class BooksApp extends React.Component {
             }}>
           </div>
           <div className="book-shelf-changer">
-            { Selection({key, section}) }
+            { Selection({id, section}) }
           </div>
         </div>
         <div className="book-title">{title}</div>
@@ -188,10 +188,10 @@ class BooksApp extends React.Component {
     );
   }
 
-  Selection({key, section}) {
+  Selection({id, section}) {
     const { Select, Option } = this,
       items = this.SelctionItems(),
-      onChange = this.onSelectionChange.bind(this, key);
+      onChange = this.onSelectionChange.bind(this, id);
 
     return Select({items, defaultValue: section, onChange}, {Option});
   }
@@ -199,19 +199,19 @@ class BooksApp extends React.Component {
   SelctionItems() {
     const { none, currentlyReading, wantToRead, read } = SectionEnum,
       items = [
-        {key: '', value: '', name: 'Move to..', disabled: true},
-        {key: currentlyReading.key, value: currentlyReading.key, name: currentlyReading.name},
-        {key: wantToRead.key, value: wantToRead.key, name: wantToRead.name},
-        {key: read.key, value: read.key, name: read.name},
-        {key: none.key, value: none.key, name: none.name},
+        {id: '', value: '', name: 'Move to..', disabled: true},
+        {id: currentlyReading.id, value: currentlyReading.id, name: currentlyReading.name},
+        {id: wantToRead.id, value: wantToRead.id, name: wantToRead.name},
+        {id: read.id, value: read.id, name: read.name},
+        {id: none.id, value: none.id, name: none.name},
       ];
 
     return items;
   }
 
-  onSelectionChange(key, value) {
+  onSelectionChange(id, value) {
     const { books } = this.state,
-      index = books.findIndex(x => x.key === key);
+      index = books.findIndex(x => x.id === id);
 
     books[index].section = Number(value);
     this.setState({books});
@@ -226,12 +226,12 @@ class BooksApp extends React.Component {
     );
   }
 
-  Option({key, value, name, disabled}) {
+  Option({id, value, name, disabled}) {
     const props = {disabled};
 
     return (
       <option
-        key={key}
+        key={id}
         value={value}
         {...props}
       >{name}</option>
--- src/utils/SectionEnum.js
+++ src/utils/SectionEnum.js
@@ -6,10 +6,10 @@ const Enum = {
   },
 
   Props = {
-    [Enum.NONE]: {key: Enum.NONE, enum: 'NONE', name: 'None'},
-    [Enum.CURRENTLY_READING]: {key: Enum.CURRENTLY_READING, enum: 'CURRENTLY_READING', name: 'Currently Reading'},
-    [Enum.WANT_TO_READ]: {key: Enum.WANT_TO_READ, enum: 'WANT_TO_READ', name: 'Want to Read'},
-    [Enum.READ]: {key: Enum.READ, enum: 'READ', name: 'Read'}
+    [Enum.NONE]: {id: Enum.NONE, enum: 'NONE', name: 'None'},
+    [Enum.CURRENTLY_READING]: {id: Enum.CURRENTLY_READING, enum: 'CURRENTLY_READING', name: 'Currently Reading'},
+    [Enum.WANT_TO_READ]: {id: Enum.WANT_TO_READ, enum: 'WANT_TO_READ', name: 'Want to Read'},
+    [Enum.READ]: {id: Enum.READ, enum: 'READ', name: 'Read'}
   };
 
 export default {
