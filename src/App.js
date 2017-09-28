4f07b3bdfced61fe14fdea4c5fc4120d24118354
Ernst Salzmann <Ernst.Salzmann@datameer.com>
2017-09-29 00:31:47 +0200

    refactor: Section -> Shelf

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { bindAll, noop } from 'lodash';
 // import * as BooksAPI from './BooksAPI'
 import './App.css';
-import SectionEnum from './utils/SectionEnum';
+import ShelfEnum from './utils/ShelfEnum';
 
 function sortBookByTitle(x, y) {
   if (x.title < y.title) return -1;
@@ -26,43 +26,43 @@ class BooksApp extends React.Component {
         url: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
         title: 'To Kill a Mockingbird',
         author: 'Harper Lee',
-        section: SectionEnum.CURRENTLY_READING
+        shelf: ShelfEnum.CURRENTLY_READING
       }, {
         id: 2,
         url: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")',
         title: 'Ender`s Game',
         author: 'Orson Scott Card',
-        section: SectionEnum.CURRENTLY_READING
+        shelf: ShelfEnum.CURRENTLY_READING
       }, {
         id: 3,
         url: 'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")',
         title: '1776',
         author: 'David McCullough',
-        section: SectionEnum.WANT_TO_READ
+        shelf: ShelfEnum.WANT_TO_READ
       }, {
         id: 4,
         url: 'url("http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api)',
         title: 'Harry Potter and the Sorcerer`s Stone',
         author: 'J.K. Rowling',
-        section: SectionEnum.WANT_TO_READ
+        shelf: ShelfEnum.WANT_TO_READ
       }, {
         id: 5,
         url: 'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api)',
         title: 'The Hobbit',
         author: 'J.R.R. Tolkien',
-        section: SectionEnum.READ
+        shelf: ShelfEnum.READ
       }, {
         id: 6,
         url: 'url("http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api)',
         title: 'Oh, the Places You`ll Go!',
         author: 'Seuss',
-        section: SectionEnum.READ
+        shelf: ShelfEnum.READ
       }, {
         id: 7,
         url: 'url("http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api)',
         title: 'The Adventures of Tom Sawyer',
         author: 'Mark Twain',
-        section: SectionEnum.READ
+        shelf: ShelfEnum.READ
       }]
   }
 
@@ -121,13 +121,13 @@ class BooksApp extends React.Component {
     const { addBook, Bookshelf } = this,
       { books } = this.state,
 
-      bookshelves = SectionEnum.asList
-        .filter(section => section.id !== SectionEnum.NONE)
-        .map(section => Bookshelf({
-          id: section.id,
-          title: section.name,
-          section: section.id,
-          books: books.filter((book) => book.section === section.id).sort(sortBookByTitle)
+      bookshelves = ShelfEnum.asList
+        .filter(shelfEnum => shelfEnum.id !== ShelfEnum.NONE)
+        .map(shelfEnum => Bookshelf({
+          id: shelfEnum.id,
+          title: shelfEnum.name,
+          shelf: shelfEnum.id,
+          books: books.filter((book) => book.shelf === shelfEnum.id).sort(sortBookByTitle)
         }));
 
     return (
@@ -147,7 +147,7 @@ class BooksApp extends React.Component {
     )
   }
 
-  Bookshelf({id, title, section, books}) {
+  Bookshelf({id, title, shelf, books}) {
     const { Book } = this,
       booklist = books.map((book) => {
         return <li key={book.id}>{Book(book)}</li>
@@ -165,7 +165,7 @@ class BooksApp extends React.Component {
     );
   }
 
-  Book({id, url, title, author, section}) {
+  Book({id, url, title, author, shelf}) {
     const { Selection } = this;
 
     return (
@@ -179,7 +179,7 @@ class BooksApp extends React.Component {
             }}>
           </div>
           <div className="book-shelf-changer">
-            { Selection({id, section}) }
+            { Selection({id, shelf}) }
           </div>
         </div>
         <div className="book-title">{title}</div>
@@ -188,16 +188,16 @@ class BooksApp extends React.Component {
     );
   }
 
-  Selection({id, section}) {
+  Selection({id, shelf}) {
     const { Select, Option } = this,
       items = this.SelctionItems(),
       onChange = this.onSelectionChange.bind(this, id);
 
-    return Select({items, defaultValue: section, onChange}, {Option});
+    return Select({items, defaultValue: shelf, onChange}, {Option});
   }
 
   SelctionItems() {
-    const { none, currentlyReading, wantToRead, read } = SectionEnum,
+    const { none, currentlyReading, wantToRead, read } = ShelfEnum,
       items = [
         {id: '', value: '', name: 'Move to..', disabled: true},
         {id: currentlyReading.id, value: currentlyReading.id, name: currentlyReading.name},
@@ -213,7 +213,7 @@ class BooksApp extends React.Component {
     const { books } = this.state,
       index = books.findIndex(x => x.id === id);
 
-    books[index].section = Number(value);
+    books[index].shelf = Number(value);
     this.setState({books});
   }
 
