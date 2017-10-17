8533e5a3cf774952015ae46462e25763b5604cab
Ernst Salzmann <Ernst.Salzmann@datameer.com>
2017-10-17 22:20:35 +0200

    fix: formatting of Select

--- src/Select.js
+++ src/Select.js
@@ -2,30 +2,36 @@ import React from 'react'
 import { noop } from 'lodash'
 
 const Select = ({
-    // props
-    items=[],
-    defaultValue='',
-    onChange=noop,
+  // props
+  items=[],
+  defaultValue='',
+  onChange=noop,
 
-    // context
-    Option=noop
+  // context
+  Option=noop
 }={}) => (
-    <select
-        defaultValue={defaultValue}
-        onChange={(e) => onChange(e.target.value)}
-        >{items.map(Option)}</select>
+  <select
+    defaultValue={defaultValue}
+    onChange={(e) => onChange(e.target.value)}
+  >{items.map(Option)}</select>
 );
 
-const Option = ({id, value, name, disabled}) => {
-    const props = {disabled};
+const Option = ({
+  // props
+  id,
+  value,
+  name,
+  disabled
+}={}) => {
+  const props = {disabled};
 
-    return (
-        <option
-            key={id}
-            value={value}
-            {...props}
-            >{name}</option>
-    );
+  return (
+    <option
+      key={id}
+      value={value}
+      {...props}
+    >{name}</option>
+  );
 }
 
 export default (props) => Select({ ...props,
